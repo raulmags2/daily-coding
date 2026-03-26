4b0aec8b2cec4dd29a1e91975dcd2b91f32de5e3
raulmags2 <d2026008820@unifei.edu.br>
2026-03-26 08:03:01 -0300

    Add files via upload

---
+++ Learning-Projects/Sequence finder.py
@@ -0,0 +1,13 @@
+seq = input("Type your sequence:")  
+start_codon = seq.find('ATG')
+stops = ["TAA", "TAG", "TGA"]
+first_stop = -1
+for stop in stops:
+    test_stop = seq.find(stop, start_codon + 3)
+    if test_stop != -1 and (first_stop - start_codon) % 3 == 0:
+        if first_stop == -1 or test_stop < first_stop:
+            first_stop = test_stop
+print(f"The start códon starts in {start_codon}")
+print(f"The stop códon starts in {first_stop}") 
+print(f"The sequence is: {seq[start_codon : first_stop + 3]}")
+
