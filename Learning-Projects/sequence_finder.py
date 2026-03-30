4487611e4644d2fe4f2c01fc1c602acfa7f10e83
raulmags2 <d2026008820@unifei.edu.br>
2026-03-30 08:28:50 -0300

    Update sequence_finder.py

--- Learning-Projects/sequence_finder.py
+++ Learning-Projects/sequence_finder.py
@@ -7,7 +7,7 @@ for stop in stops:
     if test_stop != -1 and (first_stop - start_codon) % 3 == 0:
         if first_stop == -1 or test_stop < first_stop:
             first_stop = test_stop
-print(f"The start códon starts in {start_codon}")
-print(f"The stop códon starts in {first_stop}") 
+print(f"The start codon starts in {start_codon}")
+print(f"The stop codon starts in {first_stop}") 
 print(f"The sequence is: {seq[start_codon : first_stop + 3]}")
 
