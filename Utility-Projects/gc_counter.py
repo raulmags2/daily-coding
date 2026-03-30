360460d9f0b1e3002d84e03e7d973fdd65488e89
raulmags2 <d2026008820@unifei.edu.br>
2026-03-30 08:37:55 -0300

    Update gc_counter.py

--- Utility-Projects/gc_counter.py
+++ Utility-Projects/gc_counter.py
@@ -2,8 +2,8 @@ def calculate_gc(sequence):
     gc_count = 0 #Initialize GC counter to zero
     gc_count = sum(1 for bases in sequence if bases in 'GC') #Adds 1 to gc count if a base is g or c   
     total = len(sequence) #Calculate the total number of bases in the sequence
-    gc_percentage = gc_count / Total * 100 #Calculate the GC percentage
-    return GC_percentage
+    gc_percentage = gc_count / total * 100 #Calculate the GC percentage
+    return gc_percentage
 fname = input("Type the file name:")
 full_sequence = ("")
 fh = open(fname)
@@ -15,4 +15,4 @@ for line in fh:
     lines.append(line)
 full_sequence =  "".join(lines)
 gc = calculate_gc(full_sequence)
-print(f"The GC percentage is {GC:.2f}%")
+print(f"The GC percentage is {gc:.2f}%")
