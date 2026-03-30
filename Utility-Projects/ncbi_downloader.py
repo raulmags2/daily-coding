531ff94b87cfc4a7db5956aea8dfb4006ea4482c
raulmags2 <d2026008820@unifei.edu.br>
2026-03-30 08:50:29 -0300

    Update ncbi_downloader.py

--- Utility-Projects/ncbi_downloader.py
+++ Utility-Projects/ncbi_downloader.py
@@ -1,7 +1,7 @@
 from Bio import Entrez #Import the library
-Entrez.email = input("Type yout email:") #Asks the user email, used to enter in NCBI
+Entrez.email = input("Type your email:") #Asks the user email, used to enter in NCBI
 handle = Entrez.efetch(db = input("type of data:"), id = input("id:"), rettype = input("Data format:"), retmode = "text") #Search in the ncbi for the sequence 
-print("Searching for NBCI data...")
+print("Searching for NCBI data...")
 data = handle.read() #Read the sequence
 handle.close() #close the connection
 print("download completed")
