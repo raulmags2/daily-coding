a4970f324d06143e4ec084b546e0a184fe93912d
raulmags2 <d2026008820@unifei.edu.br>
2026-03-30 08:31:47 -0300

    Update and rename thirdy_degree_function.py to third_degree_function.py

--- Utility-Projects/third_degree_function.py
+++ Utility-Projects/third_degree_function.py
@@ -1,9 +1,9 @@
 import math
 def test_roots(a, b, c, d):
-    Root_test = -abs(d)#turn the 'd' value into negative, to calculate the divisors without errors
-    while Root_test <= abs(d):
-        if Root_test != 0 and d % Root_test == 0:
-            x = Root_test
+    root_test = -abs(d)#turn the 'd' value into negative, to calculate the divisors without errors
+    while root_test <= abs(d):
+        if root_test != 0 and d % Root_test == 0:
+            x = root_test
             y = a * (x ** 3) + b * (x ** 2) + c * x + d
             if y == 0:
                 x1 = x
@@ -12,15 +12,15 @@ def test_roots(a, b, c, d):
         Root_test = Root_test + 1
 #calculates all the values of the second degree polynomial
 def second_degree(a, b, c, x1):
-    Quad_a = a
-    Quad_b = x1 * a + b
-    Quad_c = x1 * Quad_b + c
-    delta = Quad_b ** 2 - (4 * Quad_a * Quad_c)
+    quad_a = a
+    quad_b = x1 * a + b
+    quad_c = x1 * quad_b + c
+    delta = quad_b ** 2 - (4 * quad_a * quad_c)
     if delta < 0:
-        print ("this function do not has real matematic roots")
+        print ("this function has no real matematic roots")
     else:#calculate the roots of polynomial, reducing to the first degree form
-        x2 = ((-Quad_b + math.sqrt(delta)) / (2 * Quad_a))
-        x3 = ((-Quad_b - math.sqrt(delta)) / (2 * Quad_a))
+        x2 = ((-quad_b + math.sqrt(delta)) / (2 * quad_a))
+        x3 = ((-quad_b - math.sqrt(delta)) / (2 * quad_a))
         return x2, x3
 try: #ask to the user about her polynomial.
     a_value = float(input("what is the a value?"))
@@ -28,12 +28,12 @@ try: #ask to the user about her polynomial.
     c_value = float(input("what is the c value?"))
     d_value = float(input("what is the d value?"))
 except ValueError:#avoid errors, if the user type letters in the input
-    print("you have to type a number!")
+    print("Invalid input. Please enter a numeric value.")
 x1 = test_roots(a_value, b_value, c_value, d_value)
 x2, x3 = second_degree(a_value, b_value, c_value, x1)
-print (f"the value of polynomial roots is:")
-print (f"the value of x1 its {x1}")
-print (f"The value of x2 its {x2}")
-print (f"The value of x3 its {x3}")
+print (f"the polinomial roots are:")
+print (f"x1 = {x1}")
+print (f"x1 = {x2}")
+print (f"x3 = {x3}")
 print (f"the polynomial, reduced to the first-degree form is:")
 print (f"(x-{x1})(x-{x2})(x-{x3})")
