9ba9fbbd6f99a51a40f39b1f9d288b172f12499a
raulmags2 <d2026008820@unifei.edu.br>
2026-03-30 08:19:38 -0300

    Delete gc_counter.py

--- gc_counter.py
+++
@@ -1,9 +0,0 @@
-def calculate_gc(sequence):
-    gc_count = 0 #Initialize GC counter to zero
-    gc_count = sum(1 for bases in sequence if bases in 'gc') #adds 1 to gc count if a base is g or c   
-    Total = len(sequence) #calculate the total of bases in the sequence
-    GC_percentage = gc_count / Total * 100 #Calculate the GC percentage
-    return GC_percentage
-Seq = input("Type your Sequence:").lower() #Ask the user for the DNA sequence to analyze
-result = calculate_gc(Seq)
-print(f"the GC percentage in this sequence is {result:.2f}%")
