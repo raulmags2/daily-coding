bed37e25071ab4a97dee4f2d3fd4ee7be5d595a5
raulmags2 <d2026008820@unifei.edu.br>
2026-03-30 08:41:17 -0300

    Delete Utility-Projects/ncbi_downloader.py

--- Utility-Projects/ncbi_downloader.py
+++
@@ -1,9 +0,0 @@
-from Bio import Entrez
-Entrez.email = input("Type yout email:")
-handle = Entrez.efetch(db = input("type of data:"), id = input("id:"), rettype = input("Data format:"), retmode = "text") 
-print("Searching for NBCI data...")
-data = handle.read()
-handle.close()
-print("download completed")
-with open(input("Archive name:"), "w") as f:
-    f.write(data)
