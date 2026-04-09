1be1d9c9d0319005029cb4f8d2cadd33faeb9d99
raulmags2 <270363974+raulmags2@users.noreply.github.com>
2026-04-09 16:17:33 -0400

    Update Ids_finder.py - remove unused variable, add comments

--- Ids_finder.py
+++ Ids_finder.py
@@ -4,13 +4,12 @@ fh = open(fname)#Open the file
 lines = [] #create an ampty list
 final_list = []
 for line in fh: #for each line in fh, removes the whitespace in the right and, if the line starts with >, adds to the list
-    line = line.rstrip()
+    line = line.rstrip() 
     if re.search(r"^>", line):
         lines.append(line)
-data = "\n".join(lines)
-for i, Id in enumerate(lines):
+for i, Id in enumerate(lines): #Loops the enumerate list and adds Id and the number of the id and a new line
     Id = (f"\nId {i}\n {Id}")
     final_list.append(Id)
-final_data = "".join(final_list)
-with open (input("Type the new file name:"), "w") as f:
+final_data = "".join(final_list) #adds the final_list to a string
+with open (input("Type the new file name:"), "w") as f:#write the string to a data file
     f.write(final_data)
